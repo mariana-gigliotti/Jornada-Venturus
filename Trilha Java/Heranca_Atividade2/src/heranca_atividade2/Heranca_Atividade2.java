e05b9c0e3bdda69945824ae9c887ad4cbe495218
Mariana Gigliotti <gigliotti.mariana@gmail.com>
2023-01-26 00:12:12 -0300

    Revisão dos exercícios

--- Trilha Java/Heranca_Atividade2/src/heranca_atividade2/Heranca_Atividade2.java
+++ Trilha Java/Heranca_Atividade2/src/heranca_atividade2/Heranca_Atividade2.java
@@ -5,6 +5,14 @@ public class Heranca_Atividade2 {
     public static void main(String[] args) {
         Homem h1 = new Homem();
         Cao c1 = new Cao();
+        Papagaio p1 = new Papagaio();
+        Gato g1 = new Gato();
+        
+        h1.nome = "Mário";
+        h1.comer();
+        h1.idade = 28;
+        h1.fome = true;
+        h1.comer();
         
         c1.nome = "floco";
         c1.comer();
@@ -12,7 +20,18 @@ public class Heranca_Atividade2 {
         c1.fome = true;
         c1.comer();
         
-        System.out.println(h1.getNome());
+        p1.nome = "Loro";
+        p1.comer();
+        p1.idade = 25;
+        p1.fome = true;
+        p1.comer();
+        
+        g1.nome = "Lila";
+        g1.comer();
+        g1.idade = 12;
+        g1.fome = true;
+        g1.comer();
+
         
     }
     
--- Trilha Java/Heranca_Atividade2/src/heranca_atividade2/Homem.java
+++ Trilha Java/Heranca_Atividade2/src/heranca_atividade2/Homem.java
@@ -15,29 +15,5 @@ public class Homem extends Animal{
             System.out.println("Não está com fome");
         }
     }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public boolean isFome() {
-        return fome;
-    }
-
-    public void setFome(boolean fome) {
-        this.fome = fome;
-    }
     
 }
